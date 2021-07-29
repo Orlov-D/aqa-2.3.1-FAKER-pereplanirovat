33f2f6168b7afa69ca9e1e3a5171ff5319b911ef
unknown <orlovdimas92@mail.ru>
2021-07-29 23:45:30 +0300

    New generator, LocalDate

--- src/test/java/ru/netology/patterns/CardDeliveryTest.java
+++ src/test/java/ru/netology/patterns/CardDeliveryTest.java
@@ -4,10 +4,12 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.generator.DataGenerator;
+import generator.DataGenerator;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 import static com.codeborne.selenide.Condition.*;
@@ -23,17 +25,15 @@ public class CardDeliveryTest {
     @Test
     public void shouldSubmitRequest() {
         SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=city] input").setValue("Абакан");
+        form.$("[data-test-id=city] input").setValue(DataGenerator.Registration.generateByCard("ru").getCity());
         form.$("[data-test-id=date] input").doubleClick();
         form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, 3);
-        form.$("[data-test-id=date] input").setValue(new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime()));
-        form.$("[data-test-id=name] input").setValue(DataGenerator.generateName("ru"));
-        form.$("[data-test-id=phone] input").setValue(DataGenerator.generatePhone("ru"));
+        form.$("[data-test-id=date] input").setValue(DataGenerator.iNeedThisDate());
+        form.$("[data-test-id=name] input").setValue(DataGenerator.Registration.generateByCard("ru").getName());
+        form.$("[data-test-id=phone] input").setValue(DataGenerator.Registration.generateByCard("ru").getPhone());
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button] .button__content").click();
-        $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime())));
+        $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.iNeedThisDate()));
         form.$("[role=button] .button__content").click();
         $("[data-test-id=replan-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?\n" +
                 "\n" +
@@ -42,5 +42,6 @@ public class CardDeliveryTest {
         $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + $("[data-test-id=date] input").getValue()));
 
     }
+
 
 }
